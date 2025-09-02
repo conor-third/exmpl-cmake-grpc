b09020f7af7ae52d0d3e37b7227cf6553c0d3616
Conor McGuigan <conor.mcguigan@third.com>
2025-09-02 16:15:02 +0100

    removed original example code

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,7 +1,5 @@
 #include <memory>
 #include <thread>
-#include <myproto/address.pb.h>
-#include <myproto/addressbook.grpc.pb.h>
 #include <myproto/GapGun.pb.h>
 #include <myproto/GapGun.grpc.pb.h>
 
@@ -22,24 +20,8 @@ MessageRequest MakeMessageRequest(const std::string& id, const MessageType type,
 
 int main(int argc, char* argv[])
 {
-    // Setup request
-    expcmake::NameQuerry query;
-    expcmake::Address result;
-    query.set_name("John");
-
     // Call
     auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
-    std::unique_ptr<expcmake::AddressBook::Stub> stub = expcmake::AddressBook::NewStub(channel);
-    grpc::ClientContext context;
-    grpc::Status status = stub->GetAddress(&context, query, &result);
-
-    // Output result
-    std::cout << "I got:" << std::endl;
-    std::cout << "Name: " << result.name() << std::endl;
-    std::cout << "City: " << result.city() << std::endl;
-    std::cout << "Zip:  " << result.zip() << std::endl;
-    std::cout << "Street: " << result.street() << std::endl;
-    std::cout << "Country: " << result.country() << std::endl;
 
     TokenRequest tok;
     TokenRequest tok_res;
@@ -47,7 +29,7 @@ int main(int argc, char* argv[])
 
     std::unique_ptr<GapGunRPCService::Stub> gg_stub = GapGunRPCService::NewStub(channel);
     grpc::ClientContext gg_context;
-    status = gg_stub->SetToken(&gg_context, tok, &tok_res);
+    ::grpc::Status status = gg_stub->SetToken(&gg_context, tok, &tok_res);
     std::cout << "Token response: " << tok_res.token() << '\n';
     
     grpc::ClientContext gg_context_bi;
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,5 @@
 #include <algorithm>
 #include <mutex>
-#include <myproto/address.pb.h>
-#include <myproto/addressbook.grpc.pb.h>
-#include <myproto/addressbook.grpc.pb.h>
 #include <myproto/GapGun.grpc.pb.h>
 #include <myproto/GapGun.pb.h>
 
@@ -10,21 +7,6 @@
 #include <grpcpp/server_builder.h>
 
 #include <iostream>
-
-class AddressBookService final : public expcmake::AddressBook::Service {
-    public:
-        virtual ::grpc::Status GetAddress(::grpc::ServerContext* context, const ::expcmake::NameQuerry* request, ::expcmake::Address* response)
-        {
-            std::cout << "Server: GetAddress for \"" << request->name() << "\"." << std::endl;
-
-            response->set_name("Peter Peterson");
-            response->set_zip("12345");
-            response->set_country("Superland");
-            
-            return grpc::Status::OK;
-        }
-
-};
 
 class GapGunRPCServiceImpl final : public GapGunRPCService::Service
 {
@@ -69,9 +51,7 @@ int main(int argc, char* argv[])
     grpc::ServerBuilder builder;
     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
 
-    AddressBookService my_service;
     GapGunRPCServiceImpl gg_service;
-    builder.RegisterService(&my_service);
     builder.RegisterService(&gg_service);
 
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
